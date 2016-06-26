f235c0bce934ca6cdd67f2bee6cd7d2b6b3aa66d
S K <xz64@users.noreply.github.com>
2016-06-26 21:40:54 +0000

    test: ensure plugin structure is correct

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var path = require('path');
 var licensePlugin = function(opts) {
 };
 
-licensePlugin.prototype.apply = function(compiler) {};
+licensePlugin.prototype.apply = function(compiler) {
+  compiler.plugin('done', function() {
+  });
+};
 
 module.exports = licensePlugin;
--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,54 @@
 'use strict';
 
 var test = require('tape');
+var sinon = require('sinon');
 var CleanWebpackPlugin = require('../index');
+
+function createPlugin() {
+  return new CleanWebpackPlugin();
+}
+
+function createCompiler() {
+  return { 
+    plugin: sinon.spy()
+  };
+}
 
 test('the plugin exists', function(t) {
   t.ok(CleanWebpackPlugin);
   t.end();
 });
 
+test('the plugin can be instantiated with the new operator', function(t) {
+  t.ok(createPlugin());
+  t.end();
+});
+
 test('the plugin has an apply function on its prototype', function(t) {
   t.ok(typeof CleanWebpackPlugin.prototype.apply === 'function');
+  t.end();
+});
+
+test('the plugin should invoke compiler.plugin', function(t) {
+  var plugin = createPlugin();
+  var compiler = createCompiler();
+  plugin.apply(compiler);
+  t.ok(compiler.plugin.called);
+  t.end();
+});
+
+test('the plugin should invoke compiler.plugin when done', function(t) {
+  var plugin = createPlugin();
+  var compiler = createCompiler();
+  plugin.apply(compiler);
+  t.ok(compiler.plugin.calledWith('done'));
+  t.end();
+});
+
+test('the plugin provides a callback after compilation is done', function(t) {
+  var plugin = createPlugin();
+  var compiler = createCompiler();
+  plugin.apply(compiler);
+  t.ok(typeof compiler.plugin.args[0][1] === 'function');
   t.end();
 });
