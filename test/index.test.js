b22311631c3e1d86c923fd2976a8087e39cfe211
S K <xz64@users.noreply.github.com>
2016-06-26 21:14:30 +0000

    test: ensure plugin exists

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,14 @@
 'use strict';
 
 var test = require('tape');
+var CleanWebpackPlugin = require('../index');
 
-test('placeholder', function(t) {
-  t.pass('placeholder passed');
+test('the plugin exists', function(t) {
+  t.ok(CleanWebpackPlugin);
+  t.end();
+});
+
+test('the plugin has an apply function on its prototype', function(t) {
+  t.ok(typeof CleanWebpackPlugin.prototype.apply === 'function');
   t.end();
 });
